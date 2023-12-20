8a08301494721b391656d924c694fcd45d998317
stastnan <nik.stastna@gmail.com>
2023-12-20 15:53:04 +0100

    Working with a multidimensional array

---
+++ index.js
@@ -0,0 +1,42 @@
+const dangerArray = [
+  ["🎅", "👺"],
+  [
+    ["🎅", "🦁"],
+    ["👹", "🎅"],
+  ],
+  [
+    [
+      ["🎅", "🐻"],
+      ["🧌", "🎅"],
+    ],
+    [
+      ["🐯", "🎅"],
+      ["🎅", "😈"],
+    ],
+  ],
+];
+
+// from multidimentional array to one-dimentional
+function simpleArray(arr) {
+  return arr.reduce((accumulator, current) => {
+    return accumulator.concat(
+      Array.isArray(current) ? simpleArray(current) : current
+    );
+  }, []);
+}
+
+// checking all elements of the array
+function saveSanta(arr) {
+  const easyDangerArray = simpleArray(arr);
+  const goodArray = [];
+
+  for (const creature of easyDangerArray) {
+    if (creature === "🎅") {
+      goodArray.push("🎅");
+    }
+  }
+  // to remember: return statement can't be directly in the loop - it would immediately terminate the code. It has to be return statement of the function itself
+  return goodArray;
+}
+
+console.log(saveSanta(dangerArray));
